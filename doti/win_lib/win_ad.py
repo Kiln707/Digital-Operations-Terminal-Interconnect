4e4976bb5fd95573562cabe761290371f21efebb
kiln707 <steven.guy.swanson@gmail.com>
2019-01-17 16:28:52 -0800

    work on working with user accounts

--- doti/win_lib/win_ad.py
+++ doti/win_lib/win_ad.py
@@ -1,14 +1,33 @@
-from pyad import aduser
+from pyad import *
 
-def getDomainUser(name):
-    try:
-        return aduser.ADUser.from_cn(name)
-    except invalidResults:
-        return None
+from pyad import aduser, adbase, adquery
+from pyad.pyadexceptions import invalidResults
+
+def init_pyad(fqdn, username, password):
+    adbase.set_defaults(ldap_server=fqdn, username=username, password=password, user=aduser.ADUser.from_cn(username))
+
+def QueryAD(attributes=[], where='', OU=''):
+    q = adquery.ADQuery()
+    q.execute_query(attributes=attributes, where_clause=where, base_dn=OU)
+    return q.get_results()
+
+def getDomainUser(name, OU):
+    attributes=['distinguishedName']
+    where="sAMAccountName = '%s'"%name
+    return aduser.ADUser.from_dn(list(QueryAD(attributes, where, OU))[0]['distinguishedName'])
 
 def domainUserExists(username):
-    try:
-        aduser.ADUser.from_cn(username)
-        return True
-    except invalidResults:
-        return False
+    q = adquery.ADQuery()
+    q.execute_query(attributes=['distinguishedName'],
+        where_clause="sAMAccountName = '%s'"%username,
+        base_dn = "OU=Accounts, DC=solano, DC=cc, DC=ca, DC=us")
+    return len(list(q.get_results())) == 1
+
+
+if __name__ == '__main__':
+    q = adquery.ADQuery()
+    q.execute_query(attributes=['distinguishedName'],
+        where_clause="sAMAccountName = 'sswanson'",
+        base_dn = "DC=solano, DC=cc, DC=ca, DC=us")
+    for r in q.get_results():
+        print(r)
--- doti/win_lib/win_local_user_groups.py
+++ doti/win_lib/win_local_user_groups.py
@@ -1,6 +1,33 @@
-import win32net
-from .win_ad import getDomainUser, domainUserExists
+import win32net, json
+from win_ad import getDomainUser, domainUserExists
 
-def addUserToGroup(group, username, domain=None):
-    if domainUserExists(username):
-        win32net.NetGroupAddUser(domain, group, username)
+def userExists(username, domain=None):
+    try:
+        win32net.NetUserGetInfo(domain, username, 0)
+        return True
+    except:
+        return False
+
+def getDomainUser(username, domain=None, info_level=0):
+    if userExists(username, domain):
+        return win32net.NetUserGetInfo(domain, username, info_level)
+    else:
+        return None
+
+def localGroupExists(groupname):
+    try:
+        win32net.NetLocalGroupGetInfo(None, groupname, 0)
+        return True
+    except:
+        return False
+
+def addUserToLocalGroup(group, username, domain=None):
+    if domain and userExists(username, domain):
+        win32net.NetLocalGroupAddMembers(None, group, 3, [{'domainandname':domain+'\\'+username}])
+    elif userExists(username):
+        win32net.NetLocalGroupAddMembers(None, group, 3, [{'domainandname':username}])
+
+def removeUserFromLocalGroup(group, username)
+
+if __name__ == '__main__':
+    addUserToLocalGroup('Administrators', 'testing')
